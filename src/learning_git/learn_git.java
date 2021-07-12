4e5aeca8af0d585fc1d1c64642b8763ad140e31f
shivapillai <shivapillai@DESKTOP-AVTTE7C>
2021-07-12 16:52:34 +0530

    2nd Push to Github

--- src/learning_git/learn_git.java
+++ src/learning_git/learn_git.java
@@ -4,8 +4,7 @@ public class learn_git {
 
 	public static void main(String[] args) {
 		System.out.println("Trying this for the first time");
-		
-
+		System.out.println("This is the second push");
 	}
 
 }
